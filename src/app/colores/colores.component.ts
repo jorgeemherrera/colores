56f1c71549d1be924381c4bcd6c624730ae9bfac
Jorge Eliecer <jorge.m.h@outlook.com>
2019-10-03 00:38:14 -0500

    creacion diseño flexbox y scss

---
+++ src/app/colores/colores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-colores',
+  templateUrl: './colores.component.html',
+  styleUrls: ['./colores.component.scss']
+})
+export class ColoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
