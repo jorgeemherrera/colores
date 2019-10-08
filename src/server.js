5ca579a43a503b47367e775f0e4c44fba2095914
Jorge Eliecer <jorge.m.h@outlook.com>
2019-10-07 20:07:00 -0500

    modificando favicon y server express por error en heroku

--- src/server.js
+++ src/server.js
@@ -6,9 +6,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/colores'));
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/dist/colores/index.html'));
+app.use(express.static(__dirname + '/dist/colores'));
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/colores/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
